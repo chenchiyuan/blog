583339725d7f72dc9932b96081b3f07d2baee991
chenchiyuan <chenchiyuan03@gmail.com>
2014-01-09 16:14:55 +0800

    新增了grow效果，增加了home页面

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ from blog import const
 from blog import views
 
 urlpatterns = patterns('',
+    url(r'^$', views.BlogListView.as_view(), name="home"),
     url(r'^posts/$', views.BlogListView.as_view(), name="blogs"),
     url(r'^posts/%s/$' %const.URL_ID, views.BlogDetailView.as_view(), name="blog_detail"),
     url(r'^slides/%s/$' %const.URL_ID, views.SlideDetailView.as_view(), name="slide_detail"),
